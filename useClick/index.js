e7cffb45667b39a4e6054fb8744c5100f0412495
DevYuns <ydhun93@gmail.com>
2021-03-05 11:49:05 +0900

    Init project

---
+++ useClick/index.js
@@ -0,0 +1,1 @@
+import {useClick} from './useClick';
---
+++ useTitle/useTitle.js
@@ -0,0 +1,10 @@
+export const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    console.log(htmlTitle);
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
